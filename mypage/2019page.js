429b01dbce64ecd8d8a152420a1b0e90d69ddd0c
pault929 <45674688+pault929@users.noreply.github.com>
2019-01-03 19:25:27 -0500

    Add files via upload

---
+++ mypage/2019page.js
@@ -0,0 +1,20 @@
+console.log("Happy New Year")
+for (var i = 1; i <= 10; i++) {
+console.log(i)}
+
+var	temperature = 39
+if (temperature > 40){
+	console.log("It is a nice day")
+}else{
+	console.log("It is cold")
+}
+
+function multiply13(x){
+	return x *13
+}
+console.log(multiply13(15))
+
+var students= ["jane", "mike", "amanda","jim"]
+	for (var i = 0; i < students.length; i++) {
+		console.log(students[i])
+	}
